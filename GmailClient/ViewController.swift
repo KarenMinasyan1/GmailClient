ba68e2c6aafee154a131d79b5be6948b2c258fa7
karenminasyan123 <65548718+karenminasyan123@users.noreply.github.com>
2021-08-21 22:26:01 +0400

    Initial Commit

---
+++ GmailClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GmailClient
+//
+//  Created by Karen Minasyan on 21.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
