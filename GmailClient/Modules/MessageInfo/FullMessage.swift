edf5dc2020bdc12683fb248b463c8c49c20cc93f
karenminasyan123 <42841763+KarenMinasyan1@users.noreply.github.com>
2021-08-26 11:47:29 +0400

    Unnecessary properties removed.

--- GmailClient/Modules/MessageInfo/FullMessage.swift
+++ GmailClient/Modules/MessageInfo/FullMessage.swift
@@ -6,31 +6,19 @@
 //
 
 struct FullMessageResponse: Codable {
-    let historyId: String?
     let id: String?
-    let internalDate: String?
-    let labelIds: [String]?
     let payload: Payload?
-    let sizeEstimate: Int?
-    let snippet: String?
-    let threadId: String?
+
 }
 
 struct Payload: Codable {
-    let body: Body?
-    let filename: String?
     let headers: [Header]?
-    let mimeType: String?
-    let partId: String?
     let parts: [Part]?
 }
 
 struct Part: Codable {
     let body: Body?
-    let filename: String?
-    let headers: [Header]?
     let mimeType: String?
-    let partId: String?
 }
 
 struct Header: Codable {
@@ -39,6 +27,5 @@ struct Header: Codable {
 }
 
 struct Body: Codable {
-    let size: Int?
     let data: String?
 }
